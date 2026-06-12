87c8b4f6e09d70ce9e9f30cbfaef9720ca995e19
Eyob Mulugeta <164791538+eyoba45@users.noreply.github.com>
2026-06-13 00:21:45 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { useState, useRef, useEffect } from "react";
 import axios from "axios";
 import "./App.css";
 
-const API = "http://127.0.0.1:8000/api";
+const API = "http://0.0.0.0:8000/api";
 
 function getFileIcon(filename) {
   if (!filename) return "📄";
